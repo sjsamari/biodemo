4fceb7cc1790d6bc24e664dd4e2fb5bf7b0bffcb
Ubuntu <ubuntu@None.novalocal>
2018-11-30 01:23:48 +0000

    Add maxlen

--- biodemo/biodemo.py
+++ biodemo/biodemo.py
@@ -61,6 +61,11 @@ def parse_args():
         default=DEFAULT_MIN_LEN,
         help='Minimum length sequence to include in stats (default {})'.format(
             DEFAULT_MIN_LEN))
+    parser.add_argument(
+        '--maxlen',
+        metavar='N',
+        type=int,
+        help = 'Maximum length sequence to include in stats (default = None)')
     parser.add_argument('--version',
                         action='version',
                         version='%(prog)s ' + PROGRAM_VERSION)
@@ -114,7 +119,7 @@ class FastaStats(object):
                 self.num_seqs, self.num_bases, self.min_len, self.max_len,
                 self.average)
 
-    def from_file(self, fasta_file, minlen_threshold=DEFAULT_MIN_LEN):
+    def from_file(self, fasta_file, minlen_threshold=DEFAULT_MIN_LEN,maxlen_threshold=None):
         '''Compute a FastaStats object from an input FASTA file.
 
         Arguments:
@@ -130,7 +135,8 @@ class FastaStats(object):
         min_len = max_len = None
         for seq in SeqIO.parse(fasta_file, "fasta"):
             this_len = len(seq)
-            if this_len >= minlen_threshold:
+            if this_len >= minlen_threshold and 
+              (maxlen_threshold is None or this_len <= maxlen_threshold):
                 if num_seqs == 0:
                     min_len = max_len = this_len
                 else:
@@ -197,7 +203,7 @@ def process_files(options):
                     print(stats.pretty(fasta_filename))
     else:
         logging.info("Processing FASTA file from stdin")
-        stats = FastaStats().from_file(sys.stdin, options.minlen)
+        stats = FastaStats().from_file(sys.stdin, options.minlen,options.maxlen)
         print(stats.pretty("stdin"))
 
 
